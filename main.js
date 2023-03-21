7ee5beb99e1234a12b3e9e7972098d5cef72fd98
vinceye <vinceye.cm@gmail.com>
2023-03-21 14:03:48 -0500

    initial pageLoad function added

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("Hello World");
+(()=>{"use strict";(()=>{const e=document.querySelector("#content");let t=document.createElement("div");t.className="headline",t.textContent="Welcome to Chez Kolache!";let n=document.createElement("div");n.className="nav-bar",n.textContent="Home Menu Contact";let o=document.createElement("div");o.className="restaurant-image",o.textContent="Image placeholder";let a=document.createElement("div");a.className="restaurant-info",a.textContent="This is some information about our kolaches",e.append(t,n,o,a)})(),console.log("Hello World")})();
